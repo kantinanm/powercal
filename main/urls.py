aff4890e1df209d8148b4e7423b3f2df04314dc9
basicgts <basicgts@gmail.com>
2022-01-31 16:23:34 +0700

    create route post , ajax using with jquery

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
     path('',views.index, name="index"),
+    path('post/ajax/process', views.process_backend, name = "process_backend"),
     path('demo',views.demo, name="demo"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,10 +1,23 @@
 import re
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.core import serializers
 
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
+def process_backend(request):
+    if is_ajax(request=request) and request.method == "POST":
+        #request.POST
+        #request.POST.get('firstnamevalue')
+
+        return JsonResponse({"success": "test"}, status=200)
+    return JsonResponse({"error": "error-msg"}, status=400)
+
 def demo(request):
     return render(request,'demo.html')
+
+def is_ajax(request):
+    return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
