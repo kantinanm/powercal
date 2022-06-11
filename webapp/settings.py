82e9a095f43aa6fe0141059c5dc84d8de154c6f5
basicgts <kantinanm@nu.ac.th>
2022-06-11 15:04:58 +0700

    fix setting.py for python collect static

--- webapp/settings.py
+++ webapp/settings.py
@@ -123,14 +123,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+#custom env
 CHART_PATH=env('CHART_PATH')
 DSS_PATH=env('DSS_PATH')
 CSV_PATH=env('CSV_PATH')
